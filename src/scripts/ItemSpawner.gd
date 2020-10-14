86e9c164dbdc5206dc38b16953caccd4224906c5
Kati Baker <Bakenshake09@gmail.com>
2020-10-14 17:47:42 -0400

    Forgot to Randomize lawl
    - Added randomize so the grid is different each time now
    - Something is wrong with my offsets so need to tweak spawn locations

--- src/scripts/ItemSpawner.gd
+++ src/scripts/ItemSpawner.gd
@@ -5,10 +5,8 @@ var worldItems = []
 var columns = 3
 var rows = 10
 
-var y_offset = 800
-var x_offset = 270
-var currItemPos_X
-var currItemPos_Y
+var y_offset = 200
+var x_offset = 165
 
 var butter = load("res://scenes/Items/Butter.tscn")
 var jam = load("res://scenes/Items/Jam.tscn")
@@ -16,7 +14,7 @@ var jam = load("res://scenes/Items/Jam.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	worldItems = createArray(columns, rows)
-	printArray()
+	#printArray()
 	spawnItems(worldItems, columns, rows)
 
 func createArray(columns, rows):
@@ -28,8 +26,9 @@ func createArray(columns, rows):
 		
 		#randomly assigns a symbol to the world grid
 		for y in range(rows):
+			randomize()
 			worldItems[x][y] = itemOptions[randi() % itemOptions.size()]
-			#print("[%s]" % worldItems[x][y])
+			print("[%s]" % worldItems[x][y])
 			
 	return worldItems
 
@@ -37,21 +36,24 @@ func spawnItems(worldItems, columns, rows):
 	var item
 	for x in range(columns):
 		for y in range(rows):
+			print("Item Number: %s" % worldItems[x][y])
 			if worldItems[x][y] == "B":
 				item = butter.instance()
+				#print("Item is butter")
 			if worldItems[x][y] == "J":
 				item = jam.instance()
+				#print("Item is jam")
 			#If it's the first item to spawn
 			if x == 0 && y == 0:
-				print("First item")
+				#print("First item")
 				item.position.x = x_offset
 				item.position.y = y_offset
-				print(item.position)
+				print("Item position is: %s" % item.position)
 			else:
-				print("Not first item")
+				#print("Not first item")
 				item.position.x = x * x_offset
 				item.position.y = y * y_offset
-				print(item.position)
+				print("Item position is: %s" % item.position)
 			add_child(item)
 
 #For debugging					
