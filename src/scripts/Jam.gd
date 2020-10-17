7f249a50e321d0fac2027d1d07a21b4152bf6599
Kati Baker <Bakenshake09@gmail.com>
2020-10-17 00:50:51 -0400

    Jam Tweaks
    - used friction instead of linear velocity on the jam
    -- makes the game harder

--- src/scripts/Jam.gd
+++ src/scripts/Jam.gd
@@ -6,10 +6,9 @@ var bottom_destroy_height : float = 0
 func _on_Area2D_body_entered(_body: RigidBody2D):
 	#update speed here
 	if _body && _body.name == "Toast":
-		_body.linear_velocity.y = _body.linear_velocity.y * 0.75
-	
+		#_body.linear_velocity.y = _body.linear_velocity.y * 0.75
+		_body.set_friction(0.5)
 	queue_free()
-
 
 func _ready():
 	bottom_destroy_height = get_viewport_rect().size.y * 2 + 200
