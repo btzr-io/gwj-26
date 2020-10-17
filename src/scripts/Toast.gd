a86e095224f95fdfde39a8784969f890c596fb2e
btzr-io <btzr.io@gmail.com>
2020-10-16 23:17:05 -0500

    add basic UI and screen exit detection

--- src/scripts/Toast.gd
+++ src/scripts/Toast.gd
@@ -6,6 +6,8 @@ export var steer_acceleration : float = 4000
 signal start_rising
 signal start_falling
 
+var falling = false
+
 var next_physics_process_position : Vector2 = Vector2()
 var next_physics_process_should_set_position : bool = false
 var horizontal_force : float = 0
@@ -16,13 +18,14 @@ func _ready():
 	$Origin.set_as_toplevel(true)
 	$Marker.set_as_toplevel(true)
 	$Max_score.set_as_toplevel(true)
+	# $VisibilityEnabler2D.connect("screen_exited", self, "handle_screen_exit")
 	connect("start_falling", self, "handle_falling")
-
+	
 func handle_falling():
 	var score = global_position.distance_to($Origin.global_position)
 	# Check for max score
-	if score >= GM.max_score:
-		GM.max_score = score
+	if score > GM.max_score:
+		GM.max_score = stepify(score / 100, 0.1)
 		$Max_score.visible = true
 		$Max_score.global_position.y = global_position.y
 	else:
@@ -30,9 +33,16 @@ func handle_falling():
 		$Marker.visible = true
 		$Marker.global_position.y = global_position.y
 		# print_debug("falling!! " + str(score / 100, "ft"))
-	# Update score
-	GM.score = score
 
+
+func update_score():
+	# Update score
+	var score = global_position.distance_to($Origin.global_position)
+	GM.score = stepify(score / 100, 0.1)
+
+func _process(delta):
+	if !falling:
+		update_score()
 
 func _physics_process(delta):
 	var horizontal_steering : float = 0
@@ -47,8 +57,10 @@ func _physics_process(delta):
 	if linear_velocity.y * last_vertical_velocity < 0 : #direction of vertical velocity changed
 		if linear_velocity.y < 0:
 			emit_signal("start_rising")
+			falling = false
 		else:
 			emit_signal("start_falling")
+			falling = true
 	last_vertical_velocity = linear_velocity.y
 
 
@@ -58,7 +70,7 @@ func _integrate_forces(state):
 		state.transform.origin = next_physics_process_position
 	state.linear_velocity.x += horizontal_force
 	horizontal_force = 0
-	state.linear_velocity.y = clamp(state.linear_velocity.y, -2500, 2500)
+	state.linear_velocity.y = clamp(state.linear_velocity.y, -2700, 2700)
 
 func set_physics_position(pos : Vector2):
 	next_physics_process_position = pos
