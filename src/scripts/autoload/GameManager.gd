80b4d45a08a74ec21841a86180a28fbd1076ec42
Kati Baker <Bakenshake09@gmail.com>
2020-10-17 16:38:34 -0400

    Game Over Screen
    - game over screen once the toast leaves the screen

--- src/scripts/autoload/GameManager.gd
+++ src/scripts/autoload/GameManager.gd
@@ -7,6 +7,10 @@ var max_score = 0
 var last_score = 0
 var combo_count = 0
 
+var player_name
+
+var game_over = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
