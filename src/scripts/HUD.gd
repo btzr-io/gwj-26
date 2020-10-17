f85001532d47f0bb1ea3ed6aaf7b80ef4ecf8ed9
btzr-io <btzr.io@gmail.com>
2020-10-17 02:34:43 -0500

    better format for score

--- src/scripts/HUD.gd
+++ src/scripts/HUD.gd
@@ -8,7 +8,13 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+	
+func format_score(n):
+	var formated = str(n).pad_zeros(1)
+	if formated.split('.').size() <= 1:
+		formated += ".0"
+	return formated + " M"
+	
 func _process(delta):
-	$Score.text = str( GM.score, " M" )
+	$Score.text = format_score(GM.score)
 	$Combo.text = str(GM.combo_count,"x")
