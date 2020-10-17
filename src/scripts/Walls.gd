226b69f6f50cf0cd56a585f8825b4e0eb3d0eb17
btzr-io <btzr.io@gmail.com>
2020-10-16 20:16:44 -0500

    change walls opacity

--- src/scripts/Walls.gd
+++ src/scripts/Walls.gd
@@ -11,6 +11,8 @@ export(int) var expand_level setget set_expand_level
 func expand_walls(level):
 	$Background/ColorRect.rect_size.y += 1920  * level
 	$Background/ColorRect.rect_position.y -= 1920 * level
+	$Color/ColorRect.rect_size.y += 1920  * level
+	$Color/ColorRect.rect_position.y -= 1920 * level
 	for layer in layers:
 		# Left side
 		var wall_left_path = "Left" + "/" + layer + "/Wall"
