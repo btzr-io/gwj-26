8bf995efdec750eae849c54ca61f7ff9b2955c25
btzr-io <btzr.io@gmail.com>
2020-10-18 10:16:57 -0500

    save player name and disable submit if text is empty

--- src/scripts/HUD.gd
+++ src/scripts/HUD.gd
@@ -7,12 +7,12 @@ extends CanvasLayer
 
 const LEADERBOARD = preload("res://addons/silent_wolf/Scores/Leaderboard.tscn")
 
-var check_box = null
+var new_name = null
 var submit_button = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	submit_button = $GameOver/User_form/Submit.connect("button_down", self, "handle_submit")
-	check_box = $GameOver/User_form/EnterName.get_text()
+	$GameOver/User_form/EnterName.text = GM.player_name
 	
 	
 func _process(delta):
@@ -32,25 +32,21 @@ func _process(delta):
 		get_tree().quit()
 		
 	if $GameOver.visible && Input.is_action_pressed("ui_accept"):
+		GM.game_over = false	
 		get_tree().reload_current_scene()
-		GM.game_over = false
-		$GameOver.hide()		
-
-func handle_submit():
-	GM.player_name = $GameOver/User_form/EnterName.get_text()
 	
-	if GM.player_name == check_box:
-		$GameOver/NeedName.popup()
-	if GM.player_name != check_box:
-		#print("PLAYER NAME IS:")
-		#print(GM.player_name)
+	# Prevent empty submit
+	if $GameOver/User_form/EnterName.text.empty() && !$GameOver/User_form/Submit.disabled:
+		$GameOver/User_form/Submit.disabled = true
+	elif !$GameOver/User_form/EnterName.text.empty() && $GameOver/User_form/Submit.disabled:
+		$GameOver/User_form/Submit.disabled = false
+		
+func handle_submit():
+	if !$GameOver/User_form/EnterName.text.empty():
+		GM.player_name = $GameOver/User_form/EnterName.text
 		SilentWolf.Scores.persist_score(GM.player_name, GM.last_score)
 		yield(SilentWolf.Scores.get_high_scores(), "sw_scores_received") 
 		#print("Scores: " + str(SilentWolf.Scores.scores))
 		var leaderboard = LEADERBOARD.instance()
 		add_child(leaderboard)
-		#get_tree().reload_current_scene()
-
-func _on_Button_pressed():
-	$User_form/NeedName.hide()
-		
+		# get_tree().reload_current_scene()
